f16740fb6eabcf44a5e931469b8090e3c1d27d65
Cian Jinks <cjinks99@gmail.com>
2020-05-20 17:55:36 +0100

    Added charts page

---
+++ src/main/java/ie/tcd/pavel/ExerciseChartsPage.java
@@ -0,0 +1,12 @@
+package ie.tcd.pavel;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("charts")
+public class ExerciseChartsPage extends VerticalLayout {
+
+    public ExerciseChartsPage() {
+        // Add your page code here
+    }
+}
