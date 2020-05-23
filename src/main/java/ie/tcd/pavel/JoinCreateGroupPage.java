d75fed57bcc53ce3455a41e6a27400ba7b06cd34
cppavel <petrukhp@tcd.ie>
2020-05-23 07:50:23 +0300

    added what Anton wanted to commit in his last force push

--- src/main/java/ie/tcd/pavel/JoinCreateGroupPage.java
+++ src/main/java/ie/tcd/pavel/JoinCreateGroupPage.java
@@ -1,81 +1,80 @@
 package ie.tcd.pavel;
 
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.charts.model.Label;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Input;
+import com.vaadin.flow.component.html.NativeButton;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.PWA;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Route("group")
 public class JoinCreateGroupPage extends VerticalLayout {
 
-	MongoDBOperations database;
-	private boolean isNewGroup = false;
-	
 	public JoinCreateGroupPage()
 	{
-		database = BeanUtil.getBean(MongoDBOperations.class);
-
 		Button createGroup = new Button ("Create a Group");
 		Button joinGroup = new Button ("Join a Group");
 		Button confirm = new Button ("Confirm");
 		HorizontalLayout horLay = new HorizontalLayout (createGroup,joinGroup);
 		add(horLay);
-		
-		Dialog dialog = new Dialog();
-		TextField groupNameIn = new TextField();
-		TextField groupPassIn = new TextField();
-		TextField newGroupNameIn = new TextField();
-		TextField newGroupPassIn = new TextField();
-		Div head1 = new Div();
-		Div head2 = new Div();
 
-		confirm.addClickListener(buttonClickEvent -> {
-			if(isNewGroup) {
-				// User is creating a new group
-				// Check if the group already exists: if(database)
-				if(groupNameIn.getValue() != null && groupPassIn.getValue() != null) {
-					if(!database.groupExists(groupNameIn.getValue())) {
-						database.insertGroup(groupNameIn.getValue(), groupPassIn.getValue(), TemporarySessionHandler.getCurrentUser());
-					}
-				}
-			} else {
-				// User is attempting to join a group
-				if(groupNameIn.getValue() != null && groupPassIn.getValue() != null) {
-					if (database.groupExists(groupNameIn.getValue(), groupPassIn.getValue())) {
-						database.insertGroup(groupNameIn.getValue(), groupPassIn.getValue(), TemporarySessionHandler.getCurrentUser());
-					}
-				}
-			}
-		});
-		
-		VerticalLayout verLay = new VerticalLayout (head1, groupNameIn, head2, groupPassIn, confirm);
-		dialog.add(verLay);
-		
-		dialog.setWidth("400px");
-		dialog.setHeight("200px");
+		Dialog dialog1 = new Dialog();
+		Dialog dialog2 = new Dialog();
+		TextField entry1 = new TextField();
+		TextField entry2 = new TextField();
+		TextField entry3 = new TextField();
+		TextField entry4 = new TextField();
+		Div newGroupName = new Div();
+		Div newGroupPass = new Div();
+		Div joinGroupName = new Div();
+		Div joinGroupPass = new Div();
+		newGroupName.setText("Enter new Group's name: ");
+		newGroupPass.setText("Enter new Group's password: ");
+		joinGroupName.setText("Enter name of group you would like to join: ");
+		joinGroupPass.setText("Enter password: ");
 
+		VerticalLayout verLay1 = new VerticalLayout (newGroupName,entry1,newGroupPass,entry2);
+		VerticalLayout verLay2 = new VerticalLayout (joinGroupName,entry3,joinGroupPass,entry4);
+
+		dialog1.setWidth("400px");
+		dialog1.setHeight("300px");
+		dialog2.setWidth("400px");
+		dialog2.setHeight("300px");
+//
 		createGroup.addClickListener(event -> {
-			head1.setText("New Group Name:");
-			head2.setText("New Group Password:");
-			isNewGroup = true;
-		    dialog.open();
-		    groupNameIn.getElement().callJsFunction("focus");
+			dialog1.open();
+			dialog1.add(verLay1);
+			dialog1.add(confirm);
+			confirm.addClickListener( event1 -> {
+				dialog1.close();
+
+			});
+
 		});
-		
+
 		joinGroup.addClickListener(event -> {
-			head1.setText("Group Name:");
-			head2.setText("Group Password:");
-			isNewGroup = false;
-		    dialog.open();
-		    groupNameIn.getElement().callJsFunction("focus");
+			dialog2.open();
+			dialog2.add(verLay2);
+			dialog2.add(confirm);
+			confirm.addClickListener( event1 -> {
+				dialog2.close();
+
+			});
 		});
-		
-		
-		
+
+
+
 
 	}
 }
